991e21512adc7dd837a30657c7de4a78d046df1a
Whitesoul293 <157641062+Whitesoul293@users.noreply.github.com>
2024-06-19 15:48:06 +0200

    middleware per inserimento annuncio

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PublicController;
 
 Route::get('/', [PublicController::class,'homepage'])->name('home');
-Route::get('/inserisci-annuncio', [PublicController::class, 'insertAdd'])->name('insertAdd');
+Route::get('/inserisci-annuncio', [PublicController::class, 'insertAdd'])->middleware(['auth'])->name('insertAdd');
