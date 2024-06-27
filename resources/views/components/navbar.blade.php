b056a3dca6987986142c4afa7aa76b922c4e371b
Alessio Morello <alessio.morello@hotmail.com>
2024-06-27 21:16:58 +0200

    implementato livewire nel search

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -46,22 +46,8 @@
                     <input class="form-control ms-2" type="search" name="query" placeholder="Search"
                         aria-label="Search">
                 </form> --}}
-                <form role="search" action="{{ route('article.search') }}" method="GET" class="d-flex"
-                    role="search">
-                    <div class="nav">
-                        <div class="search-nav">
-                            <input class="input" type="search" name="query" placeholder="Search"
-                            aria-label="Search">
-                            <svg viewBox="0 0 24 24" class="search__icon">
-                                <g>
-                                    <path
-                                        d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z">
-                                    </path>
-                                </g>
-                            </svg>
-                        </div>
-                    </div>
-                </form>
+
+                <livewire:search />
 
 
 
@@ -79,13 +65,14 @@
                         <ul class="dropdown-menu dropdown-menu-end">
                             @if (Auth::user()->is_revisor)
                                 <li class="nav-item">
-                                    <a class="nav-link btn btn-outline-success btn-sm position-relative w-sm-25"
+                                    <a class="dropdown-item"
                                         href="{{ route('revisor.index') }}">Zona revisore
                                     </a>
                                 </li>
                             @endif
+                            <li><hr class="dropdown-divider"></li>
                             {{-- <li><a class="dropdown-item" href="#">Impostazioni</a></li>
-                        <li><a class="dropdown-item" href="#">Gestione account</a></li> --}}
+                            <li><a class="dropdown-item" href="#">Gestione account</a></li> --}}
                             {{-- <li><hr class="dropdown-divider"></li> --}}
                             <li>
                                 <a class="dropdown-item" href="#"
