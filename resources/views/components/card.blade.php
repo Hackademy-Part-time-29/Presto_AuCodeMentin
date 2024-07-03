814284e1b0135974fcc9faf1bda3abc43fb12b8c
Alessio Morello <alessio.morello@hotmail.com>
2024-07-03 18:34:04 +0200

    Sistemato il css della card

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -14,12 +14,12 @@
 
 
 <div class="cardCustom">
-    <div class="cardContainer">
-        <article class="cardArticle">
-            <img src="{{ $article->images->isNotEmpty() ? $article->images->first()->getUrl() : 'https://picsum.photos/200' }}" class="cardImg" alt="Immagine dell'articolo {{ $article->title }}">
+    <div class="card__container">
+        <article class="card__article">
+            <img src="{{ $article->images->isNotEmpty() ? $article->images->first()->getUrl() : 'https://picsum.photos/200' }}" class="card__img" alt="Immagine dell'articolo {{ $article->title }}">
 
-            <div class="cardContent">
-                <h5 class="cardTitle">{{ $article->title }}</h5>
+            <div class="card__data">
+                <h5 class="card__title">{{ $article->title }}</h5>
                 <h6 class="price">{{$article->price}} €</h6>
                 <a href="{{route('article.show', compact('article'))}}" class="btn btnCustom">Dettagli</a>
                 <a href="{{ route('byCategory', ['category' => $article->category]) }}" class="btn btn-outline-info ms-2">{{ $article->category->name }}</a>
