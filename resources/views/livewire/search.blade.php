54f96c01941a0351a362e07b0260c1c5b188a655
Alessio Morello <alessio.morello@hotmail.com>
2024-07-05 00:34:15 +0200

    Sistemato il search della navbar

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -20,6 +20,6 @@
 <form role="search" action="{{ route('article.search') }}" method="GET" class="d-flex" role="search">
     <div class="box">
         <input class="input-search" type="search" name="query" placeholder="{{__('search.placeholder')}}" aria-label="Search">
-        <button type="submit"><i class="bi bi-search"></i></button>
+        <button class="btn-c" type="submit"><i class="bi bi-search"></i></button>
     </div>
 </form>
