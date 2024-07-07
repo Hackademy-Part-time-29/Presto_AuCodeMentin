13f3a25e005cbc667b870fff398d69d683def484
Alessio Morello <alessio.morello@hotmail.com>
2024-07-07 21:57:44 +0200

    Iniziato implemento del messaggio reset-password

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -22,6 +22,10 @@ class PublicController extends Controller
         return view('article.searched', ['articles'=>$articles, 'query'=>$query]);
     }
 
+    public function thankYou(){
+        return view('reset-password');
+    }
+
     public function setLenguage($lang){
         session()->put('locale', $lang);
         return redirect()->back();
